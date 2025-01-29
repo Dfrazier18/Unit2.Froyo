8bea0f5eda83fcfae91e273d97346651d0095c0b
Devin Frazier <Dev.fraz18@gmail.com>
2025-01-28 20:00:51 -0500

    created undefined object for the counts and created a loop to iterate through the array

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ let flavors = prompt("Please enter a list of comma-separated froyo flavors");
 function getFlavors(froyoFlavors) {
   const flavorsArray = flavors.split(",");
   const flavorCount = {};
-  console.log(flavorsArray);
+  for (let i = 0; i < flavorsArray.length; i++) {
+    const flavor = flavorsArray[i];
+    if (flavorCount[flavor] === undefined) {
+      flavorCount[flavor] = 1;
+    } else {
+      flavorCount[flavor] += 1;
+    }
+  }
+  return flavorCount;
 }
-getFlavors(flavors);
+console.log(getFlavors(flavors));
