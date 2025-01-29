f5e0ae89ac9f1f986fccd12f243de14e67f792f0
Devin Frazier <Dev.fraz18@gmail.com>
2025-01-28 19:43:03 -0500

    created flavorsArray which creates an array from the user input

---
+++ index.js
@@ -0,0 +1,7 @@
+let flavors = prompt("Please enter a list of comma-separated froyo flavors");
+function getFlavors(froyoFlavors) {
+  const flavorsArray = flavors.split(",");
+  const flavorCount = {};
+  console.log(flavorsArray);
+}
+getFlavors(flavors);
